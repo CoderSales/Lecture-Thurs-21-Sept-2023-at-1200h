518c8a8fc8c5c4f6d6533f1a511d89ddf664a939
Coder731 <stephenobrien1@gmail.com>
2023-09-21 18:27:59 +0100

    Add file
    file 5 multi if check py
    
    Functioning
    is as follows
    
    Checks three
    if conditions
    which
    can all be
    True
    
    Also add
    
    comments for
    
    python naming
    conventions
    with numbers
    at start disallowed
    
    and in file name generally
    yes

---
+++ file-5-multi-if-check.py
@@ -0,0 +1,24 @@
+x=5
+if x ==5 :
+    print('Equals 5')
+if x > 4 :
+    print('Greater than 4')
+if x >= 5:
+    print('Greater than or equal to 5')
+
+# https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjh-u6xm7yBAxX_XUEAHeFYCBgQFnoECAkQAw&url=https%3A%2F%2Fstackoverflow.com%2Fquestions%2F9090079%2Fin-python-how-to-import-filename-starts-with-a-number%23%3A~%3Atext%3DWhich%2520would%2520make%2520the%2520answer%2Ca%2520letter%2520or%2520an%2520underscore.%2522&usg=AOvVaw2xHGAtYeNMQ9TTwwWV-cds&opi=89978449
+# In python, how to import filename starts with a number
+# StackOverflow
+# Reference
+# Content: (using Google quick question dropdowns)
+# Can a Python file name start with number?
+# Which would make the answer "No, you can't. Rename the module to something that starts with a letter or an underscore."1 Feb 2012
+
+# https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwicqvinnLyBAxWuQkEAHT8vBd4QFnoECAgQAw&url=https%3A%2F%2Fstackoverflow.com%2Fquestions%2F53346450%2Fis-it-acceptable-to-have-python-package-names-with-numbers-in-it%23%3A~%3Atext%3DSo%252C%2520yes%252C%2520you%2520can%2520have%2Ccan%2520even%2520begin%2520with%2520one!&usg=AOvVaw3L1ijFRHK0oTxXSHo2uxZ1&opi=89978449
+# Is it acceptable to have python package names with numbers ...
+# StackOverflow
+# Reference
+# Content: (using Google quick question dropdowns)
+# Question is in the webpage name 4 lines up
+# Answer:
+# So, yes, you can have a number in both a project name and a module name, and the project name can even begin with one!16 Nov 2018
