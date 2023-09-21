b2a1dd2569604431b587783320f3465ae6f8da0d
Coder731 <stephenobrien1@gmail.com>
2023-09-21 19:41:46 +0100

    Add file 7   py
    Add fle
    
    which goes through
    the simple
    standard
    math operators
    
    in python
    
    called
    
    file 7
    math operators
    py

---
+++ file-7-math-operators.py
@@ -0,0 +1,14 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+print("Hello world")
+
+x=36
+
+x=x%5
+
+print(x)
+
+y=36
+print("y/5",y/5)
+
+print("7*5",7*5)
